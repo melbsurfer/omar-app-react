b51f408b11726d91ecd0e1467b4b130757364b95
melbsurfer <melbsurfer@gmail.com>
2017-02-22 20:04:45 -0500

    Filter is starting to work as intended.  Very early in the process, and it is crude...Works though.

--- client/src/ImageListFilter.js
+++ client/src/ImageListFilter.js
@@ -10,27 +10,46 @@ class ImageListFilter extends Component {
 
     this.state = {
       images: [],
-      filters: {
-        filename: 'foo',
-        id: 'bar'
-      }
+      filename: '',
+      id: ''
+      //value: ''
     }
 
+    //this.handleWfsRequest = this.handleWfsRequest.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
-    console.log(event.target.value);
+    //this.setState({filename: event.target.value});
+
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    })
+
+    console.log('state: ', this.state);
+
   }
 
-  onExecuteWfsRequest(filterItems) {
+  handleSubmit(event) {
+    console.log(this.state);
+    this.wfsRequest(this.state);
+    event.preventDefault();
+  }
 
+  wfsRequest(filterItems) {
+
+  console.log('filterItems: ', filterItems);
     //filterItems = 'Public_img460.tif';
     let filter = '';
 
     if(filterItems) {
-      console.log('items ', filterItems);
-      filter = '&filter=' + encodeURIComponent(`filename like '%${filterItems}%' `);
+      console.log('items ', filterItems.filename);
+      filter = '&filter=' + encodeURIComponent(`filename like '%${filterItems.filename}%' `);
     }
 
     // Grab the image list from the server
@@ -56,7 +75,7 @@ class ImageListFilter extends Component {
   }
 
   componentDidMount() {
-    this.onExecuteWfsRequest();
+    this.wfsRequest();
   }
 
   render() {
@@ -67,27 +86,25 @@ class ImageListFilter extends Component {
         <div>
           <div className="well">
             <div className="row">
-              <form className="form">
+              <form className="form" onSubmit={this.handleSubmit}>
                 <fieldset>
                   <legend>Filters</legend>
                 </fieldset>
                 <div className="col-md-6">
                   <div className="form-group">
                     <label htmlFor="imageId">Image Id:</label>
-                    <input type="text" className="form-control" id="ImageId" value={this.state.filters.filename} onChange={this.handleChange}/>
+                    <input type="text" className="form-control" name="id" value={this.state.imageId} onChange={this.handleChange}/>
                   </div>
                   <div className="form-group">
                     <label htmlFor="fileName">File name:</label>
-                    <input type="text" className="form-control" id="fileName" value={this.state.filters.id} onChange={this.handleChange}/>
+                    <input type="text" className="form-control" name="filename" value={this.state.filename} onChange={this.handleChange}/>
                   </div>
                 </div>
+                <button type="submit" value="Submit" className="btn btn-primary">Submit</button>
+                &nbsp;
+                <button className="btn btn-default">Reset</button>
               </form>
               <br/>
-            </div>
-            <div className="row">
-              <button onClick={()=> this.onExecuteWfsRequest()} className="btn btn-primary">Submit</button>
-              &nbsp;
-              <button className="btn btn-default">Reset</button>
             </div>
           </div>
           <div className="row">
